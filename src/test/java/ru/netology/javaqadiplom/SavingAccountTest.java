d9dc0321f6a06d1c443c317f7135f96b4318e50b
My first test <mifalem@gmail.com>
2023-12-10 15:04:14 +0500

    Add method's tests added

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,4 +18,96 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+    @Test
+    public void shouldAddLessThanMaxBalanceNearMax() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(7_999);
+
+        Assertions.assertEquals(2_000 + 7_999, account.getBalance());
+    }
+    @Test
+    public void shouldAddThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_000);
+
+        Assertions.assertEquals(2000 + 8000, account.getBalance());
+    }
+    @Test
+    public void shouldNotAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_001);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+    @Test
+    public void shouldNotAddNegativeAmount() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(-1);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+    @Test
+    public void shouldNotAddZeroAmount() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+    @Test
+    public void shouldAddMoreThanZeroAmount() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(1);
+
+        Assertions.assertEquals(2_000 + 1, account.getBalance());
+    }
+
+//    @Test
+//    public void shouldAddLessThanMaxBalance1() {
+//        SavingAccount account = new SavingAccount(
+//                5_000,
+//                1_000,
+//                10_000,
+//                5
+//        );
+//
+//        account.pay(4001);
+//
+//        Assertions.assertEquals(5_000, account.getBalance());
+//    }
 }
