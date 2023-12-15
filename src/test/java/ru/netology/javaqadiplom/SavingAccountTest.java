9ff8c071eeb4a418592056b891f4041acae344f2
My first test <mifalem@gmail.com>
2023-12-15 15:42:34 +0500

    Saving account tests added

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class SavingAccountTest {
 
     @Test
-    public void shouldAddLessThanMaxBalance() {
+    public void shouldAddLessThanMaxBalance() { //баг1 не прибавляет значение к балансу
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -16,10 +16,11 @@ public class SavingAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        Assertions.assertEquals(5_000, account.getBalance());
     }
+
     @Test
-    public void shouldAddLessThanMaxBalanceNearMax() {
+    public void shouldAddLessThanMaxBalanceNearMax() { //баг1 не прибавляет значение к балансу
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -29,10 +30,11 @@ public class SavingAccountTest {
 
         account.add(7_999);
 
-        Assertions.assertEquals(2_000 + 7_999, account.getBalance());
+        Assertions.assertEquals(9_999, account.getBalance());
     }
+
     @Test
-    public void shouldAddThanMaxBalance() {
+    public void shouldAddThanMaxBalance() { //баг1 не прибавляет значение к балансу
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -42,10 +44,11 @@ public class SavingAccountTest {
 
         account.add(8_000);
 
-        Assertions.assertEquals(2000 + 8000, account.getBalance());
+        Assertions.assertEquals(2_000 + 8_000, account.getBalance());
     }
+
     @Test
-    public void shouldNotAddMoreThanMaxBalance() {
+    public void shouldNotAddMoreThanMaxBalance() { // тест прошел, но из-за того что метод add корявый
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -57,6 +60,7 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000, account.getBalance());
     }
+
     @Test
     public void shouldNotAddNegativeAmount() {
         SavingAccount account = new SavingAccount(
@@ -70,6 +74,7 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000, account.getBalance());
     }
+
     @Test
     public void shouldNotAddZeroAmount() {
         SavingAccount account = new SavingAccount(
@@ -83,8 +88,9 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000, account.getBalance());
     }
+
     @Test
-    public void shouldAddMoreThanZeroAmount() {
+    public void shouldAddMoreThanZeroAmount() { //баг1 не прибавляет значение к балансу
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -97,17 +103,170 @@ public class SavingAccountTest {
         Assertions.assertEquals(2_000 + 1, account.getBalance());
     }
 
+    @Test
+    public void shouldNotPayLessThanMinBalance() { // баг2 остаток счета не может быть меньше минимального баланса
+        SavingAccount account = new SavingAccount(
+                5_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(4_001);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayThanMinBalance() {
+        SavingAccount account = new SavingAccount(
+                5_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(4_000);
+
+        Assertions.assertEquals(5_000 - 4_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayMoreThanMinBalance() {
+        SavingAccount account = new SavingAccount(
+                5_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(3_999);
+
+        Assertions.assertEquals(5_000 - 3_999, account.getBalance());
+
+    }
+
+    @Test
+    public void shouldPayThanBetweenMinMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                7_680,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(4_325);
+
+        Assertions.assertEquals(7_680 - 4_325, account.getBalance());
+        System.out.println(7680 - 4325);
+
+    }
+
+    @Test
+    public void shouldNotPayThanNegativeBalance() { // баг3 баланс не может быть отрицательным
+        SavingAccount account = new SavingAccount(
+                7_680,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(8_325);
+
+        Assertions.assertEquals(7_680, account.getBalance());
+
+
+    }
+
+    @Test
+    public void initialBalanceCanNotBeNegative() { //баг4 при отрицательном балансе не выкидывается исключение вида IllegalArgumentException
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(-13_000, 5_000, 13_000, 13);
+        });
+    }
+
+    @Test
+    public void minBalanceCanNotBeNegative() { // баг5 при отрицательном минимальном балансе не выкидывается исключение вида IllegalArgumentException
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(13_000, -13_000, 26_000, 13);
+        });
+    }
+    @Test
+    public void maxBalanceCanNotBeNegative() {  // баг6 при отрицательном максимальном балансе не выкидывается исключение вида IllegalArgumentException
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(10_000, 1_000, -13_000, 13);
+        });
+    }
+
+    @Test
+    public void rateCanNotBeNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(10_000, 1_000, 13_000, -13);
+        });
+    }
+    @Test
+    public void rateCanBeZero() { // баг7 ставка может быть равна нулю, а исключение вида IllegalArgumentException не выкидывается
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(10_000, 1_000, 13_000, 0);
+        });
+    }
+
+    @Test
+    public void shouldCalculatePercentWhenBalancePositive() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                13_000,
+                15
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(30, account.yearChange());
+    }
+
+    @Test
+    public void shouldCalculatePercentWhenBalancePositive400() { // сначала надо править метод add
+        SavingAccount account = new SavingAccount(
+                100,
+                100,
+                13_000,
+                15
+        );
+
+        account.add(300);
+
+        Assertions.assertEquals(60, account.yearChange());
+    }
+
+    @Test
+    public void shouldCalculateIntegerPercent() { // баг8 не отбрасывается дробная часть
+        SavingAccount account = new SavingAccount(
+                110,
+                500,
+                13_000,
+                15
+        );
+
+        account.add(468);
+
+        Assertions.assertEquals(70, account.yearChange());
+    }
+
+
 //    @Test
-//    public void shouldAddLessThanMaxBalance1() {
+//    public void shouldNotCalculatePercentWhenBalanceNegative() { // баг9 так как остаток счета не может быть отрицательным как и ставка, то процент не можеть быть отрицательным
 //        SavingAccount account = new SavingAccount(
-//                5_000,
 //                1_000,
-//                10_000,
-//                5
+//                5_000,
+//                15_000,
+//                10
+//
 //        );
+//        account.pay(1200);
 //
-//        account.pay(4001);
-//
-//        Assertions.assertEquals(5_000, account.getBalance());
+//        Assertions.assertEquals(0, account.yearChange());
 //    }
+
+
 }
