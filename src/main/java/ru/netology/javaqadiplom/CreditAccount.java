bdf49b2aa575218066028c140e4be53d16feee17
My first test <mifalem@gmail.com>
2023-12-17 12:17:57 +0500

    Add method fixed

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -79,7 +79,7 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = amount;
+        balance = balance + amount;
         return true;
     }
 
