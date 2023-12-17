917f8af7a552ff233e0951931c6b22fd0173b3cd
Tinita <tino4ka.87@mail.ru>
2023-12-17 14:47:27 +0300

    Changed yearChange metod

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -111,7 +111,11 @@ public class SavingAccount extends Account {
      */
     @Override
     public int yearChange() {
-        return balance / 100 * rate;
+        if (balance < minBalance) {
+            return 0;
+        } else {
+            return (balance * rate) / 100;
+        }
     }
 
     public int getMinBalance() {
